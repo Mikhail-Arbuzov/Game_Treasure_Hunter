2c1f795fcb1b4c3b5b6c5e73560ff33c2c620bd6
Mikhail-Arbuzov <mihan-arbuzov@yandex.ru>
2021-07-31 15:15:57 +0400

    В обработчиках события кнопок on normal, on heavy, all options создал логику вывода данных , изменил sql запрос где all options. И везде установил синтезатор речи

--- RatingWindow.xaml.cs
+++ RatingWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace Game_Treasure_Hunter
         bool isSuccess;
         bool successfully;
         public Result result;// переменная для сбора данных из игры и отправки их в таблицу БД
-        Result result2;// переменная для вывода данных из таблицы БД
+        
 
         string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Programs\Game_Treasure_Hunter\Database\GameDB.mdf;Integrated Security=True";
 
@@ -172,7 +172,7 @@ namespace Game_Treasure_Hunter
                     {
                         while(reader.Read()) //построчно считываем данные
                         {
-                            result2 = new Result()
+                            Result result2 = new Result() //для вывода данных из таблицы БД
                             {
                                 name = reader.GetString(0),
                                 health = reader.GetInt32(1),
@@ -223,6 +223,178 @@ namespace Game_Treasure_Hunter
 
                 speechSynthesizer.SpeakAsync(promptBuilder);//воспроизведение речи
             }
+        }
+
+        private void OnNormalButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable dataTable;
+            PromptBuilder promptBuilder2 = new PromptBuilder();
+            string sqlExpression4 = String.Format("SELECT name, health, coins, cartridges, complexity, time FROM Results WHERE complexity = 'Normal' ORDER BY time ASC, cartridges ASC, coins DESC ");
+
+            using (SqlConnection connect2 = new SqlConnection(connectionString))
+            {
+                connect2.Open();
+                SqlCommand command4 = new SqlCommand(sqlExpression4, connect2);
+
+                using (SqlDataReader reader2 = command4.ExecuteReader())
+                {
+                    if (reader2.HasRows)//проверка данных
+                    {
+                        while (reader2.Read()) //построчно считываем данные
+                        {
+                            Result result3 = new Result()
+                            {
+                                name = reader2.GetString(0),
+                                health = reader2.GetInt32(1),
+                                coins = reader2.GetInt32(2),
+                                cartridges = reader2.GetInt32(3),
+                                complexity = reader2.GetString(4),
+                                time2 = reader2.GetTimeSpan(5)
+                            };
+                        }
+                    }
+                    else
+                    {
+                        speechSynthesizer.SpeakAsyncCancelAll();// отменяет все асинхронные операции синтеза речи
+                        MessageBox.Show("Данных нет! ", "База данных", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                }
+
+                SqlDataAdapter dataAdapter2 = new SqlDataAdapter(sqlExpression4, connect2);
+                dataTable = new DataTable();
+                dataAdapter2.Fill(dataTable);// заполняем таблицу
+                ratingGrid.ItemsSource = dataTable.DefaultView;
+
+                string firstCell2 = dataTable.Rows[0][0].ToString();
+
+                promptBuilder2.StartStyle(new PromptStyle()// настройка речи
+                {
+                    Emphasis = PromptEmphasis.Reduced,//низкий уровень выделения текста
+                    Rate = PromptRate.Medium,// средняя скорость речи
+                    Volume = PromptVolume.Loud // высокая громкость речи
+                });
+                promptBuilder2.AppendText("Выигрывает");//добавляю текст
+                promptBuilder2.AppendBreak(TimeSpan.FromMilliseconds(200));//пауза
+                promptBuilder2.AppendText(String.Format("{0}", firstCell2), PromptEmphasis.Strong);// называет имя из поля name
+                promptBuilder2.EndStyle();
+
+                speechSynthesizer.SpeakAsync(promptBuilder2);//воспроизведение речи
+            }
+        }
+
+        private void OnHardButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable dataTable2;
+            PromptBuilder promptBuilder3 = new PromptBuilder();
+            string sqlExpression5 = String.Format("SELECT name, health, coins, cartridges, complexity, time FROM Results WHERE complexity = 'Hard' ORDER BY time ASC, cartridges ASC, coins DESC ");
+
+            using (SqlConnection connect3 = new SqlConnection(connectionString))
+            {
+                connect3.Open();
+                SqlCommand command5 = new SqlCommand(sqlExpression5, connect3);
+
+                using (SqlDataReader reader3 = command5.ExecuteReader())
+                {
+                    if (reader3.HasRows)//проверка данных
+                    {
+                        while (reader3.Read()) //построчно считываем данные
+                        {
+                            Result result4 = new Result()
+                            {
+                                name = reader3.GetString(0),
+                                health = reader3.GetInt32(1),
+                                coins = reader3.GetInt32(2),
+                                cartridges = reader3.GetInt32(3),
+                                complexity = reader3.GetString(4),
+                                time2 = reader3.GetTimeSpan(5)
+                            };
+                        }
+                    }
+                    else
+                    {
+                        speechSynthesizer.SpeakAsyncCancelAll();// отменяет все асинхронные операции синтеза речи
+                        MessageBox.Show("Данных нет! ", "База данных", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                }
+
+                SqlDataAdapter dataAdapter3 = new SqlDataAdapter(sqlExpression5, connect3);
+                dataTable2 = new DataTable();
+                dataAdapter3.Fill(dataTable2);// заполняем таблицу
+                ratingGrid.ItemsSource = dataTable2.DefaultView;
+
+                string firstCell3 = dataTable2.Rows[0][0].ToString();
+
+                promptBuilder3.StartStyle(new PromptStyle()// настройка речи
+                {
+                    Emphasis = PromptEmphasis.Reduced,//низкий уровень выделения текста
+                    Rate = PromptRate.Medium,// средняя скорость речи
+                    Volume = PromptVolume.Loud // высокая громкость речи
+                });
+                promptBuilder3.AppendText("Победил");//добавляю текст
+                promptBuilder3.AppendBreak(TimeSpan.FromMilliseconds(200));//пауза
+                promptBuilder3.AppendText(String.Format("{0}", firstCell3), PromptEmphasis.Strong);// называет имя из поля name
+                promptBuilder3.EndStyle();
+
+                speechSynthesizer.SpeakAsync(promptBuilder3);//воспроизведение речи
+            }
+        }
+
+        private void AllOptionsButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable dataTable3;
+            PromptBuilder promptBuilder4 = new PromptBuilder();
+            string sqlExpression6 = String.Format("SELECT name, health, coins, cartridges, complexity, time FROM Results ORDER BY cartridges ASC, time ASC, coins DESC ");
+
+            using (SqlConnection connect4 = new SqlConnection(connectionString))
+            {
+                connect4.Open();
+                SqlCommand command6 = new SqlCommand(sqlExpression6, connect4);
+                using (SqlDataReader reader4 = command6.ExecuteReader())
+                {
+                    if (reader4.HasRows)//проверка данных
+                    {
+                        while (reader4.Read()) //построчно считываем данные
+                        {
+                            Result result4 = new Result()
+                            {
+                                name = reader4.GetString(0),
+                                health = reader4.GetInt32(1),
+                                coins = reader4.GetInt32(2),
+                                cartridges = reader4.GetInt32(3),
+                                complexity = reader4.GetString(4),
+                                time2 = reader4.GetTimeSpan(5)
+                            };
+                        }
+                    }
+                    else
+                    {
+                        speechSynthesizer.SpeakAsyncCancelAll();// отменяет все асинхронные операции синтеза речи
+                        MessageBox.Show("Данных нет! ", "База данных", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                }
+
+                SqlDataAdapter dataAdapter4 = new SqlDataAdapter(sqlExpression6, connect4);
+                dataTable3 = new DataTable();
+                dataAdapter4.Fill(dataTable3);// заполняем таблицу
+                ratingGrid.ItemsSource = dataTable3.DefaultView;
+
+                string firstCell4 = dataTable3.Rows[0][0].ToString();
+
+                promptBuilder4.StartStyle(new PromptStyle()// настройка речи
+                {
+                    Emphasis = PromptEmphasis.Reduced,//низкий уровень выделения текста
+                    Rate = PromptRate.Medium,// средняя скорость речи
+                    Volume = PromptVolume.Loud // высокая громкость речи
+                });
+                promptBuilder4.AppendText("Чемпион");//добавляю текст
+                promptBuilder4.AppendBreak(TimeSpan.FromMilliseconds(200));//пауза
+                promptBuilder4.AppendText(String.Format("{0}", firstCell4), PromptEmphasis.Strong);// называет имя из поля name
+                promptBuilder4.EndStyle();
+
+                speechSynthesizer.SpeakAsync(promptBuilder4);//воспроизведение речи
+
+            }
+
         }
     }
 }
