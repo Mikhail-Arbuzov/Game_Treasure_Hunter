de18aa30accf1c702d24ffbf14d3899915b1f787
Mikhail-Arbuzov <mihan-arbuzov@yandex.ru>
2021-08-10 18:06:55 +0400

    настроил отображения всех окон приложения  на экране .При старте приложения добавил заставку

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,11 @@ namespace Game_Treasure_Hunter
     /// </summary>
     public partial class App : Application
     {
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            SplashScreen splashScreen = new SplashScreen("backgroundForest2.jpg"); // при загрузки приложения отображается заставка на экране
+            splashScreen.Show(false);
+            splashScreen.Close(TimeSpan.FromSeconds(6));// время на ее закрытие
+        }
     }
 }
